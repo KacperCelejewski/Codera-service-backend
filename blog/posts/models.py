8064ed8159d8b9a03c2e68bbea10acca349b3255
Kacper Celejewski <kacpercele@gmail.com>
2025-02-11 08:14:30 +0100

    Refactor: Add abstract base model 'SluggedModel' for automatic slug generation

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,25 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+import abc
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
+class SlugModel(models.Model):
+    title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+
+class Category(SlugModel):
+
+    class Meta:
+        verbose_name_plural = "categories"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+class Tag(SlugModel):
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
+
+
+class PostManager(models.Manager):
+    def published(self):
+        return self.get_queryset().filter(published=True)
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -29,6 +55,10 @@ class Post(models.Model):
     )
     tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
     published = models.BooleanField(default=False)
+    objects = PostManager()
+
+    class Meta:
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.title
