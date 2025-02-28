0c7a22f48a0a12b9e37f57dc7e90243987d75305
Kacper Celejewski <kacpercelejewski@mac.home>
2025-02-28 14:33:50 +0100

    fix/auth folder removed bcs of unused code inherited from authtoken verification instead of jwttoken

--- auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import ObtainAuthToken
-from .views import RegisterView, LogoutView, CustomAuthToken
-
-urlpatterns = [
-    path("login/", CustomAuthToken.as_view(), name="api_login"),
-    path("register/", RegisterView.as_view(), name="api_register"),
-    path("logout/", LogoutView.as_view(), name="api_logout"),
-]
